ec90a6c1ce9f35eecd188af8c2063c7a490b67ad
salman261002 <salmanalfarisi261002@gmail.com>
2024-06-26 01:38:23 +0700

    feat: update modal

--- components/lab/modal.tsx
+++ components/lab/modal.tsx
@@ -34,10 +34,10 @@ const Modal = ({
 }) => {
   return (
     <div
-      className={`fixed left-0 top-0 z-[999] flex h-screen w-screen items-center justify-center bg-black/60 p-6 pr-10 transition-all duration-100 ease-in-out ${show ? "opacity-100" : "opacity-0 pointer-events-none delay-500"}`}
+      className={`fixed left-0 top-0 z-[999] flex h-screen w-screen items-center justify-center bg-black/60 p-3 pr-7 transition-all duration-100 ease-in-out sm:p-6 sm:pr-10 ${show ? "opacity-100" : "pointer-events-none opacity-0 delay-300"}`}
     >
       <div
-        className={`relative flex w-full max-w-lg flex-col divide-y overflow-hidden rounded-md bg-popover text-popover-foreground transition-all duration-500 ease-in-out ${
+        className={`relative flex w-full max-w-lg flex-col divide-y overflow-hidden rounded-md bg-popover text-popover-foreground transition-all duration-300 ease-in-out ${
           show ? "opacity-100 delay-100" : "opacity-0"
         }`}
       >
@@ -49,29 +49,30 @@ const Modal = ({
         >
           <X size={24} />
         </Button>
-        <div className="flex flex-col gap-y-2 px-6 py-4 sm:gap-y-4">
-          <h3 className="text-xl font-semibold">Live coding with Salman</h3>
-          <div className="mt-2 h-[1px] w-full bg-border" />
-          <p className="text-base font-medium">
+        <div className="flex flex-col gap-y-3 px-6 py-4 sm:gap-y-4">
+          <h3 className="max-w-[90%] text-lg font-semibold">
+            Live coding with Salman
+          </h3>
+          <p className="text-sm font-medium text-muted-foreground">
             Experience a master at work! Put your suit on, grab a chair, and
             join me in a live coding session where I will be building a project
             from scratch.
           </p>
-          <div className="mt-2 h-[1px] w-full bg-border" />
+          <div className="mt-2 h-[1px] w-full border-b border-dashed" />
           <div className="flex flex-col gap-y-2">
             <div className="grid grid-cols-3 items-end">
-              <p className="w-fit text-base font-medium text-muted-foreground">
+              <p className="w-fit text-xs font-medium text-muted-foreground xs:text-sm sm:text-base">
                 Place
               </p>
-              <p className="col-span-2 text-base font-medium text-foreground">
+              <p className="col-span-2 text-xs font-medium text-foreground xs:text-sm sm:text-base">
                 Zoom Meeting
               </p>
             </div>
             <div className="grid grid-cols-3 items-end">
-              <p className="w-fit text-base font-medium text-muted-foreground">
+              <p className="w-fit text-xs font-medium text-muted-foreground xs:text-sm sm:text-base">
                 Time
               </p>
-              <p className="col-span-2 text-base font-medium text-foreground">
+              <p className="col-span-2 text-xs font-medium text-foreground xs:text-sm sm:text-base">
                 08:00 PM (GMT+7)
               </p>
             </div>
@@ -86,6 +87,7 @@ const Modal = ({
               onClick={closeModal}
               variant={"destructive"}
               className="w-full sm:w-fit"
+              size={"sm"}
             >
               Cancel
             </Button>
@@ -93,6 +95,7 @@ const Modal = ({
               onClick={closeModal}
               variant={"default"}
               className="w-full sm:w-fit"
+              size={"sm"}
             >
               Reserve
             </Button>
