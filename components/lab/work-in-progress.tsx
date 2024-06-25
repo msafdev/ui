db6ee3de64265b59c909839b2d025b3ef1603788
salman261002 <salmanalfarisi261002@gmail.com>
2024-06-26 02:14:58 +0700

    feat: update wip

--- components/lab/work-in-progress.tsx
+++ components/lab/work-in-progress.tsx
@@ -5,7 +5,7 @@ const WorkInProgress = () => {
     <div className="flex flex-col items-center justify-center gap-y-2">
       <TrafficCone className="h-8 w-8 text-amber-500" />
       <p className="text-center text-sm font-semibold text-foreground">
-        Currently being <br/> cooked
+        Currently being <br /> cooked 🔥
       </p>
     </div>
   );
