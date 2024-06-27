1f5b3a78469ba684ef8a1ba958fec815a13bcc82
salman261002 <salmanalfarisi261002@gmail.com>
2024-06-27 19:05:51 +0700

    feat: update not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,9 @@
 export default function NotFound() {
   return (
     <div className="flex h-auto w-full grow flex-col items-center justify-center">
-      <h1 className="text-2xl font-semibold text-accent-foreground text-center">
-        404 | <span className="font-medium text-xl">Page Not Found</span>
+      <h1 className="flex items-center gap-x-3 text-center text-xl font-semibold leading-none text-accent-foreground">
+        404
+        <span className="text-lg font-medium">Page Not Found</span>
       </h1>
     </div>
   );
